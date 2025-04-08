fcc32faafadd3b633f398bdf01b55282cfd33044
Gustavo Pessa <ghmpessa@gmail.com>
2025-04-07 22:59:20 -0300

    feat: add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
