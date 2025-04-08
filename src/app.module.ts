cf8a59cad25a8113abf85327db62a6eb2ad5221f
Gustavo Pessa <ghmpessa@gmail.com>
2025-04-07 23:00:09 -0300

    feat: add CreateAccount controller injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 
 @Module({
@@ -10,7 +11,7 @@ import { envSchema } from './env'
       isGlobal: true,
     }),
   ],
-  controllers: [],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
