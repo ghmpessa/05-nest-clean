86710642e81c3c48f3e825785344caecdb12dc47
Gustavo Pessa <ghmpessa@gmail.com>
2025-04-14 23:58:52 -0300

    refactor: add email and password attributes, add getters

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,23 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.name
+  }
+
+  get email() {
+    return this.email
+  }
+
+  get password() {
+    return this.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(props, id)
 
